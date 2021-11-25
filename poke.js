bd64dbbace02bc7bad108300ef980c7a52b3e993
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2021-11-25 07:29:58 -0500

    pokedex v=2.0

--- poke.js
+++ poke.js
@@ -1,7 +1,7 @@
 
-const API = 'https://pokeapi.co/api/v2/pokemon/';
-const pokemones = document.getElementById('pokemones');
-const info = document.getElementById('info');
+const API           = 'https://pokeapi.co/api/v2/pokemon/';
+const pokemones     = document.getElementById('pokemones');
+const info          = document.getElementById('info');
 const infoContainer = document.querySelector('.info-container');
 
 
@@ -42,17 +42,23 @@ const pokemonHTML = (pokemon)=>{
     const pokeimg = document.createElement('img');
     pokeimg.setAttribute('src', pokemon.sprites.front_default);
 
+    //especie / color
+    const pokeSpeciesURL = pokemon.species.url;
+    fetch(pokeSpeciesURL).then(response => response.json()).then(species =>{
+        pokeDiv.setAttribute('data-color', species.color.name);
+    });
+    
     pokeDiv.prepend(pokeimg);
-
-    pokeDiv.addEventListener('click', ()=>{
+    pokeDiv.addEventListener('click', (event)=>{
+        infoContainer.setAttribute('data-color', event.currentTarget.getAttribute('data-color'));
         pokeAlertInfo(pokemon);
-        infoContainer.classList.add('active');
     })
     
 }
 
 const pokeAlertInfo = (pokemon)=>{
 
+    infoContainer.classList.add('active');
     
     //imagen
     const pokeIMG = pokemon.sprites.other.dream_world.front_default;
@@ -64,11 +70,22 @@ const pokeAlertInfo = (pokemon)=>{
 
     //tipo de pokemon
     const pokeTypes = pokemon.types.map(t => t.type.name);
+    let pokeTypesHTML = '';
+    pokeTypes.forEach(tipo => {
+        pokeTypesHTML += `<span>${tipo}</span>`;
+    });
 
     //movimientos
     const pokeMoves = pokemon.moves.map(m => m.move.name);
-    console.log(pokeMoves.join());
+    const pokeMovesResumen = (pokeMoves.length > 10) ? pokeMoves.slice(0, 10).join(', ') + '...' : pokeMoves.join(', ');
 
+
+    //btn cerrar
+    const btnClose = document.createElement('a');
+    btnClose.innerHTML = '<i class="fa fa-times-circle fa-2x" aria-hidden="true"></i>';
+    btnClose.addEventListener('click',()=>{
+        cerrarModal();
+    })
 
     fetch(pokeAttackURL)
         .then(response => response.json())
@@ -77,20 +94,28 @@ const pokeAlertInfo = (pokemon)=>{
             const attackEfect = (attack.flavor_text_entries.find(element => element.language.name === 'es').flavor_text);
             const attackResponse = `${attackName}: ${attackEfect}`;
             info.innerHTML = `
-            <div><h1>${pokemon.name}</h1><div>
-            <div class="img">
-                <img src="${pokeIMG}" alt="${pokemon.name}">
-            </div>
-            <div class="imgs">
-                <div><img src="${pokeimgFront}" alt="${pokemon.name} (vista de frente)"></div>
-                <div><img src="${pokeimgBack}" alt="${pokemon.name} (vista de trasera)"></div>
-            </div>
-            <div class="info">
-                <div><b>ataque:</b> ${attackResponse}</div>
-                <div><b>tipo:</b> ${pokeTypes.join()}</div>
-            </div>
+            <div>
+                <div><h1>${pokemon.name}<small>${pokeTypesHTML}</small></h1></div>
+                <div class="img">
+                    <img src="${pokeIMG}" alt="${pokemon.name}">
+                </div>
+                <div class="imgs">
+                    <div><img src="${pokeimgFront}" alt="${pokemon.name} (vista de frente)"></div>
+                    <div><img src="${pokeimgBack}" alt="${pokemon.name} (vista de trasera)"></div>
+                </div>
+                <div class="info">
+                    <div><h3>Ataque</h3> ${attackResponse}</div>
+                    <div><h3>Movimientos</h3> ${pokeMovesResumen}</div>
+                </div>
+            </div>            
+            <div class="bg"></div>
             `;
+
+            info.prepend(btnClose);
         });
+
+    
+    
 }
 
 const cerrarModal = ()=>{
